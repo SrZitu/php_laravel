817260e86c9cdc020165fb26a7163a63bf70cddf
SrZitu <zitu094@gmail.com>
2023-02-13 10:38:33 +0600

    updated

--- practice_loop_func.php
+++ practice_loop_func.php
@@ -11,12 +11,11 @@ echo $y;
 echo PHP_EOL;
 //What will be the output of the following PHP program?
 
-for ($i = 1, $j = 1; $i <= 3, $j <= 3; ++$i, ++$j){
+for ($i = 1, $j = 1; $i <= 3, $j <= 3; ++$i, ++$j) {
     printf("%d %d ", $i, $j);
-  
 }
 echo PHP_EOL;
-for($x=0;$x<=2;print ++$x){
+for ($x = 0; $x <= 2; print ++$x) {
     print ++$x;
 }
 echo PHP_EOL;
@@ -30,28 +29,46 @@ else if ($a > 30)
     printf("PHP Program");
 echo PHP_EOL;
 
-    //call by value
-    function adder(&$str2)  
-{  
-    $str2 .= 'Call By Value';  
-}  
-$str = 'Hello ';  
-adder($str);  
-echo $str;  
+//call by value
+function adder(&$str2)
+{
+    $str2 .= 'Call By Value';
+}
+$str = 'Hello ';
+adder($str);
+echo $str;
 
 echo PHP_EOL;
 
-function increment(&$i)  
-{  
-    $i++;  
+function increment(&$i)
+{
+    $i++;
     echo $i;
     echo PHP_EOL;
-
-}  
-$i = 10;  
-increment($i);  
-echo $i; 
+}
+$i = 10;
+increment($i);
+echo $i;
 echo PHP_EOL;
 
 include_once("functions.php");
-echo "Factorial is:". isFactorial(6);
+echo "Factorial is:" . isFactorial(6);
+echo PHP_EOL;
+
+//target
+function integerExist($array, $target_sum)
+{
+
+    for ($i = 0; $i < count($array); $i++) {
+        for ($j = $i + 1; $j < count($array); $j++) {
+            if ($array[$i] + $array[$j] == $target_sum) {
+                return "True";
+            }
+        }
+    }
+    return "false";
+}
+$array = array(5,7,1,2,8,4,3);
+$target_sum = 8;
+$result = integerExist($array, $target_sum);
+echo "result is:" . "{$result}";
