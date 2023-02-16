cf1e00f33ebb3a8862c8f47950278a6702083e67
SrZitu <zitu094@gmail.com>
2023-02-16 10:02:44 +0600

    loop+fun_exercise+livetest

--- livetest.php
+++ livetest.php
@@ -13,3 +13,16 @@ else if ($marks>33 && $marks<44){
 else{
     echo "Fail"; 
 }
+// calculating gcd module 3 live test
+function gcd($a, $b) {
+    while ($b != 0) {
+        $temp = $b;
+        $b = $a % $b;
+        $a = $temp;
+    }
+    return $a;
+}
+
+
+$gcd=gcd(12,18);
+echo $gcd;
--- practice_loop_func.php
+++ practice_loop_func.php
@@ -55,7 +55,7 @@ include_once("functions.php");
 echo "Factorial is:" . isFactorial(6);
 echo PHP_EOL;
 
-//target
+//pairs of integers in array that is equal to target sum
 function integerExist($array, $target_sum)
 {
 
@@ -68,9 +68,122 @@ function integerExist($array, $target_sum)
     }
     return "false";
 }
-$array = array(5,7,1,2,8,4,3);
+$array = array(5, 7, 1, 2, 8, 4, 3);
 $target_sum = 8;
 $result = integerExist($array, $target_sum);
 echo "result is:" . "{$result}";
+echo PHP_EOL;
+
+//Count all distinct pairs with difference equal to k
+function defferenceExist($array, $k)
+{
+    $count = 0;
+
+    for ($i = 0; $i < count($array); $i++) {
+        for ($j = $i + 1; $j < count($array); $j++) {
+            if ($array[$i] - $array[$j] == $k || $array[$j] - $array[$i] == $k) {
+                $count++;
+            }
+        }
+    }
+    return $count;
+}
+$array = array(8, 12, 16, 4, 0, 20);
+$k = 4;
+$result = defferenceExist($array, $k);
+echo "Difference count pairs is:" . "{$result}";
+echo PHP_EOL;
 
 
+// count pairs in given sum
+function pairCount($arr, $sum)
+{
+    $count = 0;
+    for ($i = 0; $i < count($arr); $i++) {
+        for ($j = $i + 1; $j < count($arr); $j++) {
+            if ($arr[$i] + $arr[$j] == $sum) {
+                $count++;
+            }
+        }
+    }
+    return $count;
+}
+$arr = array(1, 5, 7, -1, 5);
+$sum = 6;
+echo "Count of pair in given sum is:" . pairCount($arr, $sum);
+echo PHP_EOL;
+
+
+//Find all pairs with a given sum
+function pairToTargetSum($array1, $array2, $target_sum)
+{
+
+    for ($i = 0; $i < count($array1); $i++) {
+        for ($j = 0; $j < count($array2); $j++) {
+            if ($array1[$i] + $array2[$j] == $target_sum) {
+                echo $array1[$i] . " " . $array2[$j] . "\n";
+            }
+        }
+    }
+}
+$array1 = array(1, 2, 3, 7, 5, 4);
+$array2 = array(0, 7, 4, 3, 2, 1);
+$target_sum = 8;
+echo "All pairs of given sum\n";
+$result = pairToTargetSum($array1, $array2, $target_sum);
+echo "{$result}";
+echo PHP_EOL;
+
+
+//Find whether an array is subset of another array
+function isSubArray($array1, $array2)
+{
+
+    for ($i = 0; $i < count($array1); $i++) {
+        for ($j = 0; $j < count($array2); $j++) {
+            if ($array1[$i] == $array2[$j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+$array1 = array(11, 1, 13, 21, 3, 7);
+$array2 = array(2, 6, 1);
+$target_sum = 8;
+if (isSubArray($array1, $array2))
+    echo "arr2[] is subset of array 1";
+else
+    echo "arr2[] is not subset of array 1";
+
+echo PHP_EOL;
+
+//Given two arrays count all pairs whose sum is an odd number
+
+function allPairsSumOdd($array1, $array2)
+{
+    $odd1 = $odd2 = 0;
+    $even1 = $even2 = 0;
+
+    for ($i = 0; $i < count($array1); $i++) {
+        if ($array1[$i] % 2) {
+            $even1++;
+        } else {
+            $odd1++;
+        }
+    }
+    for ($j = 0; $j < count($array2); $j++) {
+        if ($array2[$j] % 2) {
+            $even2++;
+        } else {
+            $odd2++;
+        }
+    }
+    $pairs = min($even1, $odd2) + min($even2, $odd1);
+    return $pairs;
+}
+$array1 = array(9, 14, 6, 2, 11);
+$array2 = array(8, 4, 7, 20);
+
+$result = allPairsSumOdd($array1, $array2);
+echo "Resulted pair is:" . $result;
