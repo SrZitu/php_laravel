346a6a9ec78d851bb5d5aaa12d81a5d03eee19b1
SrZitu <zitu094@gmail.com>
2023-03-13 15:30:59 +0600

    assign_up

--- mod06_assignment/savedata.php
+++ mod06_assignment/savedata.php
@@ -8,6 +8,7 @@ if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['password
 $name = $_POST['name'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$profile_picture = $_FILES['profile_picture'];
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     echo "Invalid email format.";
@@ -21,22 +22,25 @@ if (!isset($_FILES['profile_picture'])) {
 }
 
 $target_dir = "uploads/";
-$filename=uniqid() . basename($_FILES['profile_picture']['name']);
-$target_file = $target_dir .$filename;
+$filename = uniqid() . '_' . date('Y-m-d_H-i-s') . '_' . $profile_picture['name'];
+$target_file = $target_dir . $filename;
 
 
-if (move_uploaded_file($_FILES["profile_picture"]["tmp_name"], $target_file)) {
+if (move_uploaded_file($profile_picture["tmp_name"], $target_file)) {
     // Save user data to CSV file
-    $data = array($name, $email, $filename, date("Y-m-d H:i:s"));
+    $data = array($name, $email, $filename);
     $fp = fopen('users.csv', 'a');
-    fputcsv($fp, $data);
+    if (fputcsv($fp, $data) === false) {
+        die('Error writing in file.');
+    }
     fclose($fp);
 
     // Set cookie and start session
     session_start();
-    setcookie('username', $name, time() + 3600, '/');
+    setcookie('username', $name);
     echo "Registration successful!";
-    require_once 'readcsv.php';
+    header("Location:readcsv.php");
+    exit();
 } else {
     echo "Error uploading profile picture.";
 }
